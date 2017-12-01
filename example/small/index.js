83bd443569f58f79be0f342ba70e1f308e0046f2
Robert Monfera <monfera.robert@gmail.com>
2017-12-01 11:00:22 +0100

    simplification

--- example/small/index.js
+++ example/small/index.js
@@ -2,53 +2,32 @@ const {cfEnabled, cfLayoutOverrides, cfPlotSetup} = window['crosslink-plotly'].j
 
 const crossfiltering = ebola.settings.enableCrossfilter
 
-const config = {}
-
-const getPlotArray = root => [].slice.call(root.querySelectorAll('.js-plotly-plot'));
-
-const renderOneDiv = (idArray, verticalContainer) => {
-  const root = idArray.length ? document.getElementById(`gd_${idArray.slice(0, -1).join('_')}`) : document.body
-  const div = document.createElement('div')
-  div.setAttribute('id', `gd_${idArray.join('_')}`)
-  //if(idArray.length) div.setAttribute('class', 'js-plotly-plot')
-  if (verticalContainer) {
-    div.style['flex-direction'] = 'column'
-  }
-  root.appendChild(div)
-  return div
-}
-
-renderOneDiv([])
-
 const plots = [
   {plotContent: ebola1},
   {plotContent: ebola2},
   {plotContent: ebola3}
 ]
 
+const renderOneDiv = idArray => {
+  const root = idArray.length ? document.getElementById(`gd_${idArray.slice(0, -1).join('_')}`) : document.body
+  const div = document.createElement('div')
+  div.setAttribute('id', `gd_${idArray.join('_')}`)
+  root.appendChild(div)
+  return div
+}
+
+// render a root container
+renderOneDiv([])
+
+const renderedDivs = []
+
 plots.forEach(({plotContent}, i) => {
-
   const plotEl = renderOneDiv([`ebola${i}`], true)
-
   const crossfilterEnabled = cfEnabled(crossfiltering, plotContent);
-
-  const layout = plotContent.layout // this.scaledLayout(plotContent.layout, plotEl, setPlotContentToClientHeight);
-
-  const plotPayload = {
-    data: plotContent.data,
-    layout: Object.assign(
-      layout, // clone(layout),
-      crossfilterEnabled ? cfLayoutOverrides : {}
-    ),
-    frames: plotContent.frames,
-    config
-  };
-
-  const plotArray = getPlotArray(document);
-
-  const gdPromise = Plotly.plot(plotEl, plotPayload);
-
+  const layout = Object.assign(plotContent.layout, crossfilterEnabled ? cfLayoutOverrides : {})
+  const plot = Plotly.plot(plotEl, plotContent.data, layout);
   if (crossfilterEnabled) {
-    cfPlotSetup(Plotly, plotArray, gdPromise, plotEl, plotContent);
+    cfPlotSetup(Plotly, renderedDivs, plot, plotEl, plotContent);
   }
+  renderedDivs.push(plotEl)
 })
